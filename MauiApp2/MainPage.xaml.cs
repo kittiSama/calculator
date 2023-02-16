aab6f8baabd5d32cc1882e7daa3424d71367daa7
User <User@LAB3-3>
2023-02-16 09:29:43 +0200

    Add project files.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	int num1 = 0;
+	int num2 = 0;
+	string operation = "";
+	int useless;
+	bool flag = false;
+	public MainPage()
+	{
+		InitializeComponent();
+
+	}
+	public void inp(object sender, EventArgs args)
+    {
+		if (flag) { theoutput.Text = ""; flag = false; } 
+		string temp = ((Button)sender).Text;
+		theoutput.Text = theoutput.Text+temp;
+        if (int.TryParse(temp, out useless))
+		{
+            if (operation == "")
+            {
+                num1 = useless;
+            }
+            else
+            {
+                num2 = useless;
+            }
+        }
+		if (temp == "+" || temp == "-") operation = temp;
+		if (temp == "=")
+		{
+			if (operation == "+")
+			{
+
+				theoutput.Text = (num1 + num2).ToString();
+
+            }
+            if (operation == "-")
+            {
+
+                theoutput.Text = (num1 - num2).ToString();
+
+            }
+			num1 = 0;
+			num2 = 0;
+			operation = "";
+			flag = true;
+        }
+
+
+	}
+}
+
